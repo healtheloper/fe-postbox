1371051d423b0fa693bc038217220105d71bb469
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-03-08 14:01:43 +0900

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import { 마을 } from "./마을.js";
+import { 마을정보가져오기 } from "../src/utils.js";
 
-const a = new 마을({ 가로길이: 50, 세로길이: 50, 좌표: { x: 10, y: 10 } });
+const { results: 초기마을정보 } = await 마을정보가져오기();
 
-console.log(a);
+const dfs = (시작점, 클래스) => {
+  const 자식노드들 = [...시작점.children];
+  const 찾은노드 = 자식노드들.find((자식노드) => {
+    if (자식노드.classList.contains(클래스)) {
+      return true;
+    } else {
+      return dfs(자식노드, 클래스);
+    }
+  });
+  return 찾은노드;
+};
+const 클래스로요소찾기 = (클래스) => {
+  const 요소 = dfs(document.body, 클래스);
+};
+
+클래스로요소찾기("townMap");
+// const a = new 마을({ 가로길이: 50, 세로길이: 50, 좌표: { x: 10, y: 10 } });
+
+// console.log(a);
--- src/마을.js
+++ src/마을.js
@@ -1,16 +1,11 @@
 import { 우체통 } from "./우체통.js";
 
 export class 마을 {
-  constructor({ 가로길이, 세로길이, 좌표: { x, y } }) {
+  constructor({ 가로길이, 세로길이, 좌표: { x, y }, 자식마을 }) {
     this.가로길이 = 가로길이;
     this.세로길이 = 세로길이;
     this.좌표 = { x, y };
     this.우체통 = new 우체통();
-    this.자식마을 = this.자식마을만들기();
-    console.log(this.constructor);
-  }
-
-  자식마을만들기() {
-    return [];
+    this.자식마을 = 자식마을.map((마을정보) => new 마을(마을정보));
   }
 }
