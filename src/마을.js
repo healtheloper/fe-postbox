394d391a14e62397cf1b3ed0a06c52534d6b066e
Sangbeom Heo <mansaout@gmail.com>
2022-03-09 11:09:27 +0900

    feat: 자식마을 생성 로직 추가

--- src/마을.js
+++ src/마을.js
@@ -6,7 +6,9 @@ export class 마을 {
     this.세로길이 = 세로길이;
     this.상대좌표 = { x, y };
     this.우체통 = new 우체통();
-    this.자식마을 = 자식마을.map((마을정보) => new 마을(마을정보));
+    this.자식마을 = 자식마을?.length
+      ? 자식마을.map((마을정보) => new 마을(마을정보))
+      : [];
   }
   template() {
     return `<div class="town" style="width:${this.가로길이}px; height:${
@@ -14,7 +16,7 @@ export class 마을 {
     }px; top:${this.상대좌표.y}px; left:${this.상대좌표.x}px">${
       this.우체통 ? this.우체통.template() : ""
     }${
-      this.자식마을.length > 0
+      this.자식마을.length
         ? this.자식마을.map((자식) => 자식.template()).join("")
         : ""
     }</div>`;
